947f39845b84d56504a74e0068a302064179d8c5
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-06 13:14:02 -0300

    create design configurator page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -38,6 +38,8 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
+  loadingText?: string
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
