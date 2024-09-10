b5d39012a759692081666a916976e48d06a823c8
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-10 10:49:43 -0300

    integrating stripe payments

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -10,33 +10,70 @@ import { useMutation } from "@tanstack/react-query"
 import { ArrowRight, Check } from "lucide-react"
 import { useEffect, useState } from "react"
 import Confetti from "react-dom-confetti"
+import { createCheckoutSession } from "./actions"
+import { useRouter } from "next/navigation"
+import { useToast } from "@/hooks/use-toast"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
+import LoginModal from "@/components/LoginModal"
 
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+
+  const router = useRouter()
+  const { toast } = useToast()
+  const { id } = configuration
+  const { user } = useKindeBrowserClient()
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+
   const [showConfetti, setShowConfetti] = useState(false)
   useEffect(() => setShowConfetti(true))
 
-  const { color, model, finish,material } = configuration
+  const { color, model, finish, material } = configuration
   const tw = COLORS.find((supportedColor) => supportedColor.value === color)?.tw
 
   const { label: modelLabel } = MODELS.options.find(({ value }) => value === model)!
 
   let totalPrice = BASE_PRICE
-  if (material === "polycarbonate")  totalPrice += PRODUCT_PRICES.material.polycarbonate
+  if (material === "polycarbonate") totalPrice += PRODUCT_PRICES.material.polycarbonate
 
   if (finish === "textured") {
     totalPrice += PRODUCT_PRICES.finish.textured
   }
 
-  const {} = useMutation({
-    mutationKey:["get-checkout-session"],
-    mutationFn:
+  const { mutate: createPaymentSession } = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url)
+      else throw new Error("Unable to retrive payment URL.")
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again",
+        variant: 'destructive'
+      })
+    }
   })
+
+  const handleCheckout = () => {
+    if (user) {
+      //
+      createPaymentSession({ configId: id })
+    } else {
+      //
+      localStorage.setItem("configurationId", id)
+      setIsLoginModalOpen(true)
+    }
+
+
+  }
   return (
     <>
       <div aria-hidden="true" className="pointer-events-none select-none absolute inset-0 overflow-hidden flex  justify-center">
         <Confetti active={showConfetti} config={{ elementCount: 200, spread: 90 }} />
       </div>
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
       <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6  md:gap-x-8 lg:gap-x-12">
         <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
           <Phone className={cn(`bg-${tw}`)} imgSrc={configuration.croppedImageUrl!} />
@@ -84,7 +121,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
                     </p>
                   </div>
                 ) : null}
-                 {material === "polycarbonate" ? (
+                {material === "polycarbonate" ? (
                   <div className="flex items-center justify-between py-1 mt-2">
                     <p className="text-gray-600">Soft polycarbonate material</p>
                     <p className="font-medium text-gray-900">
@@ -102,8 +139,8 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
               </div>
             </div>
             <div className="mt-8 flex justify-end pb-12">
-                <Button disabled={true}  className="px-4 sm:px-6 lg:px-4">Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
-                </Button>
+              <Button onClick={() => handleCheckout()} className="px-4 sm:px-6 lg:px-4">Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+              </Button>
             </div>
           </div>
         </div>
